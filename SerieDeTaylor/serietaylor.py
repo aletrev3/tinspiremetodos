0c3dcdd4c9e32441797e693a9a65080ef10caf9c
aletrev3 <revalt01@outlook.com>
2025-03-21 00:11:19 +0000

    Moví archivo a la carpeta nombre_del_folder

---
+++ SerieDeTaylor/serietaylor.py
@@ -0,0 +1,23 @@
+import math
+import sympy as sp 
+
+print("Serie de Taylor")
+def derivadas (): #función que calcula las derivadas de f(x)
+    funcionO = input("Función\n") #f(x) de usuario
+    numder = input ("Número de derivadas\n")
+    numder = int(numder) #declaracion de variable como int
+    for dfo_dx in range (numder):
+        x = sp.symbols('x') #declaración de x como variable simbolica
+        der = dfo_dx = sp.diff (funcionO,x) #derivar f(x)
+        funcionO = dfo_dx #convertir la derivada en la nueva original para derivadas de mayor grado
+        num = num
+        print(" {} derivada: {}\n".format (num+1, funcionO))
+        
+      
+derivadas()
+
+
+  
+
+
+
