bd440a5eac7ff99352643ebd08ccda31a04e2d9f
aletrev3 <revalt01@outlook.com>
2025-03-22 01:01:35 +0000

    taylor

--- SerieDeTaylor/serietaylor.py
+++ SerieDeTaylor/serietaylor.py
@@ -6,12 +6,14 @@ def derivadas (): #función que calcula las derivadas de f(x)
     funcionO = input("Función\n") #f(x) de usuario
     numder = input ("Número de derivadas\n")
     numder = int(numder) #declaracion de variable como int
+    num = 0
     for dfo_dx in range (numder):
         x = sp.symbols('x') #declaración de x como variable simbolica
         der = dfo_dx = sp.diff (funcionO,x) #derivar f(x)
         funcionO = dfo_dx #convertir la derivada en la nueva original para derivadas de mayor grado
-        num = num
-        print(" {} derivada: {}\n".format (num+1, funcionO))
+        num += 1
+        print(f"{num} derivada: {funcionO}\n")
+        
         
       
 derivadas()
