6b2b85e4736e1b0eb9ed06fe2ae3918072a152fc
aletrev3 <revalt01@outlook.com>
2025-03-24 21:25:17 +0000

    serie T

--- SerieDeTaylor/serietaylor.py
+++ SerieDeTaylor/serietaylor.py
@@ -1,25 +1,33 @@
 import math
-import sympy as sp 
+import sympy as sp
 
 print("Serie de Taylor")
-def derivadas (): #función que calcula las derivadas de f(x)
-    funcionO = input("Función\n") #f(x) de usuario
-    numder = input ("Número de derivadas\n")
-    numder = int(numder) #declaracion de variable como int
+
+
+def derivadas():  # función que calcula las derivadas de f(x)
+    funcionO = input("Función\n")  # f(x) de usuario
+    numder = input("Número de derivadas\n")
+    numder = int(numder)  # declaracion de variable como int
     num = 0
-    for dfo_dx in range (numder):
-        x = sp.symbols('x') #declaración de x como variable simbolica
-        der = dfo_dx = sp.diff (funcionO,x) #derivar f(x)
-        funcionO = dfo_dx #convertir la derivada en la nueva original para derivadas de mayor grado
+    st = []
+    div = sp.symbols('div')
+    fact = sp.factorial(div)
+    numfac = 0
+    for dfo_dx in range(numder):
+        x = sp.symbols('x')  # declaración de x como variable simbolica
+        der = dfo_dx = sp.diff(funcionO, x)  # derivar f(x)
+        numfac += 1
+        divfact = fact.subs(div, numfac)
+        facfinal = divfact
+        st.append(f"(({dfo_dx})(x-a))/{divfact}")
+        funcionO = dfo_dx  # convertir la derivada en la nueva original para derivadas de mayor grado
         num += 1
         print(f"{num} derivada: {funcionO}\n")
-        
-        
-      
+
+    # for stdiv in range (numder):
+     #   stdiv += 1
+      #  std.append(f"ST= {stdiv}")
+    print(f" + ".join(st))
+
+
 derivadas()
-
-
-  
-
-
-
