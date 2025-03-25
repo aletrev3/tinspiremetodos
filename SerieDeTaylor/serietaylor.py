70ba1b0d9530a1253dd8e25372e7b9cfd344bd65
aletrev3 <revalt01@outlook.com>
2025-03-25 19:44:58 +0000

    serie de taylor terminada

--- SerieDeTaylor/serietaylor.py
+++ SerieDeTaylor/serietaylor.py
@@ -1,7 +1,8 @@
 import math
 import sympy as sp
 
-print("Serie de Taylor")
+print("Serie de Taylor\n\n")
+print("ST=f(a)+(f'(a)(x-a))+(f''(a)(x-a)^2)/2!+(f'''(a)(x-a)^3)/3!+...+f^n(a)(x-a)^n/n!\n\n")
 
 
 def derivadas():  # función que calcula las derivadas de f(x)
@@ -19,7 +20,7 @@ def derivadas():  # función que calcula las derivadas de f(x)
         numfac += 1
         divfact = fact.subs(div, numfac)
         facfinal = divfact
-        st.append(f"(({dfo_dx})(x-a))/{divfact}")
+        st.append(f"(({dfo_dx})((x-a)^{num+1}))/{divfact}")
         funcionO = dfo_dx  # convertir la derivada en la nueva original para derivadas de mayor grado
         num += 1
         print(f"{num} derivada: {funcionO}\n")
