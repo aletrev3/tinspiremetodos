f0c22f0e140b646a66cd66a6513fc2d1f92483aa
aletrev3 <revalt01@outlook.com>
2025-03-26 20:24:29 +0000

    newton raphson

--- NewtonRaphson/newtonraphson.py
+++ NewtonRaphson/newtonraphson.py
@@ -1,24 +1,33 @@
-import math
 import sympy as sp
 
-print("Newton Raphson\n\n")
-
-funcion = input("Función original =\n")  # f(x) de usuario
-x = sp.symbols('x')  # Declaración de x como variable simbólica
+print("Método de Newton-Raphson\n\n")
+funcion = input("Función original =\n")  
+x = sp.symbols('x')  
 valdex = float(input("Valor de x = "))
-derfun = dfo_dx = sp.diff(funcion, x)
-valdexf = funcion.subs(x, valdex)
-valdexfr = round(valdexf, 4)
-valdexr = derfun.subs(x, valdex)
-valdexrr = round(valdexr, 4)
-print(f"Valor de derivada con x = {valdexrr}") 
+derfun = sp.diff(funcion, x)
+error = float(input("Error máximo permitido: "))
+print(f"Derivada sin sustituir x = {derfun}")
+errrealr = float('inf') 
+i = 0 
+while errrealr > error:
+    valdexf = sp.sympify(funcion).subs(x, valdex)
+    valdexfr = round(valdexf, 4)
+    valdexr = derfun.subs(x, valdex)
+    valdexrr = round(valdexr, 4)     
+    i += 1
+    print(f"\nIteración {i}:")
+    # Imprimir resultados
+    print(f"Valor de x (A) = {valdex}")
+    print(f"Valor de la función con x (B) = {valdexfr}")
+    print(f"Valor de la derivada con x (C) = {valdexrr}") 
+    x_i = valdex - (valdexfr / valdexrr)
+    x_i = round(x_i, 4)
+    print(f"Valor de xi+1 = {x_i}")
 
-print(f"Valor de x (A)= {valdex}")
-print(f"Valor de funcion con x (B)= {valdexfr}")
-print(f"Derivada de funcion original (C) = {derfun}")
-x_i = valdex - (valdexfr / derfun)
-x_i_l = valdex - (valdexfr / derfun)
-print(f"Valor de funcion xi = {x_i_l}")
-valdex = x_i_l
+    # Calcular el error real
+    errreal = abs((x_i - valdex) / x_i)
+    errrealr = round(errreal, 4)
+    print(f"Error real = {errrealr}")
 
-#2*x^3-11.7*x^2+17.7*x-5
+    # Actualizar el valor de x para la siguiente iteración
+    valdex = x_i
